a36dae14e76d3df73fe949509232d31f78368d66
esanchep <esanchezp85@gmail.com>
2022-05-22 11:38:15 +0200

    Add Server Application code

---
+++ server/src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/greeting/{name}")
+    String greet(@PathVariable final String name) {
+        return "Hello, ".concat(name);
+    }
+
+}
